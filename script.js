5b1b2663aa847ec11ffe145caba897924f2e9859
IT012312 <it012312@MacBook-Air-2.local>
2023-03-21 18:53:49 +0530

    just folder changed

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var x, y;
-var px,py;
-px = py =0;
-
+var px, py;
+px = py = 0;
+        
+// Image of cursor
 var cursor = document.getElementById("cursor");
+        	
 var mutex = false;
 
 window.addEventListener("mouseup", function(e) {
+        
+    // gets the object on image cursor position
     var tmp = document.elementFromPoint(x + px, y + py);
     mutex = true;
     tmp.click();
     cursor.style.left = (px + x) + "px";
-    cursor.style.top = (py + y) + "px";
+    // cursor.style.top = (py + y) + "px";
 })
 
 window.addEventListener("mousemove", function(e) {
@@ -18,8 +22,8 @@ window.addEventListener("mousemove", function(e) {
     // Gets the x,y position of the mouse cursor
     x = e.clientX;
     y = e.clientY;
+        
     // sets the image cursor to new relative position
-    cursor.style.left = (px + x) + "px";
-    cursor.style.top = (py + y) + "px";
-
+    cursor.style.left = (px + x-120) + "px";
+    // cursor.style.top = (py + y) + "px";
 });
